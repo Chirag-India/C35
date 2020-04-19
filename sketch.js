1f59fcd05ff446cbf3d5f9fbf55f07740995f082
Chirag-India <61911921+Chirag-India@users.noreply.github.com>
2020-04-19 13:12:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var database;
+var position;
+var form,player,game,playerCount;
+var gameState=0;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+ 
+}
+
+function draw(){
+}
